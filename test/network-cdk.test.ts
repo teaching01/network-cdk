87b4e39449f7a4f7de7f1c1a68ca5959d996f3e1
teaching <teaching@teachingnoMac-mini.local>
2024-03-17 08:13:58 +0900

    Initial commit

---
+++ test/network-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NetworkCdk from '../lib/network-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/network-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NetworkCdk.NetworkCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
