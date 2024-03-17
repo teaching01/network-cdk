c2097b4fbe7cd189341db488623e1f2cb9ecaf0c
teaching <teaching@teachingnoMac-mini.local>
2024-03-17 16:09:02 +0900

    add: vpc

--- bin/network-cdk.ts
+++ bin/network-cdk.ts
@@ -1,21 +1,16 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { NetworkCdkStack } from '../lib/network-cdk-stack';
+import 'dotenv/config'
+import 'source-map-support/register'
+import * as cdk from 'aws-cdk-lib'
+import { NetworkCdkStack } from '../lib/network-cdk-stack'
 
-const app = new cdk.App();
+const AWS_ACCOUNT = process.env.AWS_ACCOUNT
+const AWS_REGION = process.env.AWS_REGION
+
+const app = new cdk.App()
 new NetworkCdkStack(app, 'NetworkCdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+  env: {
+    account: AWS_ACCOUNT,
+    region: AWS_REGION,
+  },
+})
--- lib/network-cdk-stack.ts
+++ lib/network-cdk-stack.ts
@@ -1,16 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib'
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import { Construct } from 'constructs'
+
+const PREFIX = 'network-cdk'
 
 export class NetworkCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'NetworkCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // vpc
+    const vpc = new ec2.Vpc(this, `${PREFIX}-vpc`, {
+      ipAddresses: ec2.IpAddresses.cidr('192.168.0.0/16'),
+      vpcName: `${PREFIX}-vpc`,
+      availabilityZones: [`${this.region}a`],
+      subnetConfiguration: [],
+      createInternetGateway: false,
+    })
   }
 }
